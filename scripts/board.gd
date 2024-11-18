cb8d538949364a6aa2450dbcf8efbae8d69874fd
SnowHawkeye <48864485+SnowHawkeye@users.noreply.github.com>
2024-11-18 01:06:14 +0100

    Prototype board loading function

--- scripts/board.gd
+++ scripts/board.gd
@@ -1,11 +1,31 @@
 extends GridContainer
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	const BLACK_SQUARE = preload("res://scenes/black_square.tscn")
+	const WHITE_SQUARE = preload("res://scenes/white_square.tscn")
+	
+	print(BLACK_SQUARE)
+	
+	var array := []
+	var board_size = 9
+	
+	columns = board_size
+	for i in range(board_size):
+		var row := []
+		for j in range(board_size):
+			var node
+			if (i%2==0 and j%2==0) or (i%2==1 and j%2==1) :
+				node = BLACK_SQUARE.instantiate()				
+			else:
+				node = WHITE_SQUARE.instantiate()
+			node.name = "(" + str(i) + "," + str(j) + ")"
+			row.append(node)
+		array.append(row)
+	make_board(array)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func make_board(squares):
+	for x in squares:
+		for y in x:
+			add_child(y)
+			
