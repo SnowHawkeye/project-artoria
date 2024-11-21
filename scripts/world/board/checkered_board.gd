ee98f3439e073121feb1549f9b172ac4bcb76f90
SnowHawkeye <hadriensalem@gmail.com>
2024-11-21 01:01:59 +0100

    Clean up board script and add assets for pieces

--- scripts/world/board/checkered_board.gd
+++ scripts/world/board/checkered_board.gd
@@ -8,19 +8,20 @@ func _ready() -> void:
 	
 	var board_array := []
 	
-	print("board_array")
-
+	# Place squares, alternate between types
 	for i in range(board_dim_y):
 		var row := []
 		for j in range(board_dim_x):
-			var node
+			var square_node
 			if (i%2==0 and j%2==0) or (i%2==1 and j%2==1) :
-				node = square_type_1.instantiate()
+				square_node = square_type_1.instantiate()
 			else:
-				node = square_type_2.instantiate()
-			node.name = "(" + str(i) + "," + str(j) + ")"
-			row.append(node)
+				square_node = square_type_2.instantiate()
+			square_node.name = "(" + str(i) + "," + str(j) + ")"
+			row.append(square_node)
 		board_array.append(row)
+	
+	# Make board using the provided array	
 	make_board(board_array)
 	
 	
